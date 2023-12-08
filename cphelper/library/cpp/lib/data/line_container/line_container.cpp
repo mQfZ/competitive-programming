5670d1f868b6e8af9e8e6a185f79a37bd70cdbc3
mQfZ <56398396+mQfZ@users.noreply.github.com>
2023-12-08 02:22:33 -0500

    Added line_container

---
+++ cphelper/library/cpp/lib/data/line_container/line_container.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma once
+
+/**
+ * Line Container
+ * Description:
+ *    Given a set of lines in the form mx + b, computes the greatest
+ *    y-coordinate for any x. Might have issues when comparing doubles.
+ * Time Complexity: O(log n)
+ * Verification: https://codeforces.com/contest/1083/submission/236297389
+ */
+
+template <typename T>
+struct line {
+    mutable T m, b, p;
+    T eval(T x) const { return m * x + b; }
+    bool operator<(const line& o) const { return m < o.m; }
+    bool operator<(T x) const { return p < x; }
+};
+
+template <typename T>
+struct line_container {
+    static const T inf = numeric_limits<T>::max();
+    multiset<line<T>, less<>> ms;
+
+    constexpr T div(const T& a, const T& b) {
+        return is_integral_v<T> ? a / b - ((a ^ b) < 0 && a % b) : a / b;
+    }
+
+    template <typename Iterator>
+    bool isect(Iterator x, Iterator y) {
+        if (y == ms.end()) return x->p = inf, 0;
+        if (x->m == y->m) x->p = x->b > y->b ? inf : -inf;
+        else x->p = div(y->b - x->b, x->m - y->m);
+        return x->p >= y->p;
+    }
+    
+    // add line y = mx + b
+    void add(T m, T b) {
+        auto z = ms.insert({m, b, 0}), y = z++, x = y;
+        while (isect(y, z)) z = ms.erase(z);
+        if (x != ms.begin() && isect(--x, y)) isect(x, y = ms.erase(y));
+        while ((y = x) != ms.begin() && (--x)->p >= y->p) isect(x, ms.erase(y));
+    }
+
+    // query max y at x-coordinate x
+    T query(T x) {
+        assert(!ms.empty());
+        return ms.lower_bound(x)->eval(x);
+    }
+
+    void clear() {
+        ms.clear();
+    }
+};
