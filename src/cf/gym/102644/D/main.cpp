f1ea6572e150cf738b7f320313506c06923e5677
mQfZ <56398396+mQfZ@users.noreply.github.com>
2024-01-03 17:33:56 -0500

    Problem Dump

--- src/cf/gym/102644/D/main.cpp
+++ src/cf/gym/102644/D/main.cpp
@@ -9,6 +9,95 @@ using namespace std;
 #endif
 
 /**
+ * Modular Integer
+ * Description:
+ *    Static/Dynamic modular integer.
+ * Verification: 
+ */
+
+template <auto _MOD>
+class modular {
+public:
+    using T = decay_t<decltype(_MOD)>;
+    static constexpr T MOD = _MOD;
+
+private:
+    T v;
+
+    template <typename U>
+    static T normalize(const U& x) {
+        T v;
+        if (-MOD <= x && x < MOD) v = static_cast<T>(x);
+        else v = static_cast<T>(x % MOD);
+        if (v < 0) v += MOD;
+        return v;
+    }
+
+    template <typename T>
+    T mod_inv(T a, T m) {
+        T x = 0, y = 1;
+        while (a != 0) {
+            T t = m / a;
+            m -= t * a;
+            x -= t * y;
+            swap(a, m);
+            swap(x, y);
+        }
+        assert(m == 1);
+        return x;
+    }
+
+public:
+    modular() : v(0) {}
+    template <typename U> modular(const U& x) : v(normalize(x)) {}
+    
+    const T& operator()() const { return v; }
+    template <typename U> explicit operator U() const { return static_cast<U>(v); }
+
+    modular& operator+=(const modular& o) { if ((v += o.v) >= MOD) v -= MOD; return *this; }
+    modular& operator-=(const modular& o) { if ((v -= o.v) < 0) v += MOD; return *this; }
+    modular& operator*=(const modular& o) { v = normalize((int64_t) v * o.v) % MOD; return *this; }
+    modular& operator/=(const modular& o) { return *this *= mod_inv(o.v, MOD); }
+    
+    friend modular operator+(const modular& a, const modular& b) { return modular(a) += b; }
+    friend modular operator-(const modular& a, const modular& b) { return modular(a) -= b; }
+    friend modular operator*(const modular& a, const modular& b) { return modular(a) *= b; }
+    friend modular operator/(const modular& a, const modular& b) { return modular(a) /= b; }
+
+    modular& operator++() { return *this += 1; }
+    modular& operator--() { return *this -= 1; }
+    modular operator++(int) { modular res(*this); *this += 1; return res; }
+    modular operator--(int) { modular res(*this); *this -= 1; return res; }
+    modular operator-() const { return modular(-v); }
+    modular operator+() const { return modular(*this); }
+
+    friend bool operator==(const modular& a, const modular& b) { return a.v == b.v; }
+    friend bool operator!=(const modular& a, const modular& b) { return a.v != b.v; }
+    friend bool operator<(const modular& a, const modular& b) { return a.v < b.v; }
+    friend bool operator<=(const modular& a, const modular& b) { return a.v <= b.v; }
+    friend bool operator>(const modular& a, const modular& b) { return a.v > b.v; }
+    friend bool operator>=(const modular& a, const modular& b) { return a.v >= b.v; }
+
+    template <typename U>
+    friend U& operator<<(U& stream, const modular& a) { return stream << a.v; }
+    template <typename U>
+    friend U& operator>>(U& stream, modular& a) { modular::T x; stream >> x; a = x; return stream; }
+
+    friend string to_string(const modular& a) { return to_string(a.v); }
+    
+    template <typename U>
+    friend modular pow(modular& a, const U& p) {
+        assert(p >= 0);
+        modular res = 1;
+        while (p) { if (p & 1) res *= a; p >>= 1; a *= a; }
+        return res;
+    }
+};
+
+constexpr const int md = 1000000007;  // 998244353;
+using mint = modular<md>;
+
+/**
  * Matrix
  * Description:
  *    Basic matrix operations (addition, subtraction, multiplication). Uses
@@ -17,7 +106,7 @@ using namespace std;
  * Time Complexity:
  *     Addition/Subtraction: O(r * c) for r by c matrix
  *     Multiplication: O(r * c * h) for r by c matrix * c by h matrix
- *     Exponentiation: O(n ^ 3 * log2(k)) for n by n matrix
+ *     Exponentiation: O(n ^ 3 * log(k)) for n by n matrix
  * Verification: https://github.com/mQfZ/competitive-programming/blob/master/src/cf/gym/102644/D/main.cpp
  */
 
@@ -79,45 +168,12 @@ public:
         return m;
     }
 
-    friend matrix pow(matrix m, long long p) {
+    friend matrix pow(matrix m, int64_t p) {
         assert(m.rows == m.cols && p >= 0);
         matrix res = make_id(m.rows);
-        for (; p > 0; p /= 2, m *= m) if (p & 1) res *= m;
+        while (p) { if (p & 1) res *= m; p >>= 1; m *= m; }
         return res;
     }
-};
-
-/**
- * Modular Integer
- * Description:
- *    Basic modular arithmetic operations (excluding division).
- * Verification: https://codeforces.com/contest/1279/submission/207090651
- */
-
-const int md = (int) 1e9 + 7;
-
-struct mint {
-    int v;
-    
-    mint() : v(0) {}
-    mint(int _v) : v(_v % md) {}
-
-    mint& operator+=(mint o) {
-        if ((v += o.v) >= md) v -= md;
-        return *this;
-    }
-    mint& operator-=(mint o) {
-        if ((v -= o.v) < 0) v += md;
-        return *this;
-    }
-    mint& operator*=(mint o) {
-        v = (int) (((long long) v * o.v) % md);
-        return *this;
-    }
-    
-    friend mint operator+(mint a, mint b) { return a += b; }
-    friend mint operator-(mint a, mint b) { return a -= b; }
-    friend mint operator*(mint a, mint b) { return a *= b; }
 };
 
 int main() {
@@ -131,5 +187,5 @@ int main() {
     a = pow(a, k);
     mint ans = 0;
     for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) ans += a[i][j];
-    cout << ans.v << '\n';
+    cout << ans << '\n';
 }
